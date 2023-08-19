2954bc15898c59ad6e1a94f95fa844d2151c3356
Donice <technocrater.pdevelopers@gmail.com>
2023-08-19 13:43:22 +0100

    Modified dependencies

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
 import './style.css';
-import {
-  addEventsToLabels, addEventsCheckBoxes, addEventsNewTasks, addEventclearAllButton,
-  deleteFromIcons,
-} from './utils.js';
+import * as todos from './crud.js';
 
-window.onload = () => {
-  let tasksArray = [];
-  let savedFormData = localStorage.getItem('tasksList');
-  savedFormData = JSON.parse(savedFormData);
-  if (savedFormData) {
-    tasksArray = savedFormData;
-    const listContainer = document.querySelector('.todo-list');
-    tasksArray.forEach((todo) => {
-      listContainer.innerHTML += `<li>
-      <div class="taskCont">
-        <input type="checkbox" class="checkBoxesTasks" id="checkbox${todo.index}">
-        <div class="task" id="taskText${todo.index}">${todo.description}</div>
-        <textarea class="textArea" maxlength="255"></textarea>
-      </div>
-      <div class="icon">&#8942</div>
-    </li>
-  `;
-    });
-  }
+document.querySelector('.enter').addEventListener('click', todos.storeTodos);
+document.querySelector('form').addEventListener('submit', todos.storeTodos);
+document.querySelector('button').addEventListener('click', todos.clearCompletedTodos);
 
-  addEventsToLabels();
-  addEventsCheckBoxes();
-  addEventsNewTasks(tasksArray);
-  addEventclearAllButton();
-  deleteFromIcons();
-};
+window.addEventListener('load', todos.createTodos);
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-
-  mode: 'development',
-
-  entry: {
-    index: './src/index.js',
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-
-  ],
-
+  mode: 'production',
+  entry: './src/index.js',
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './assets/images/favicon.ico',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './assets/images/capture.png', to: 'images' }],
+    }),
+  ],
   output: {
-
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-
-  optimization: {
-    runtimeChunk: 'single',
-  },
-
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/inline',
       },
     ],
   },
